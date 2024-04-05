0faa87288ae17b11a92117667e067c60243e630d
Watcharawish Onsanit <watcharawish@Watcharawishs-MacBook-Pro.local>
2024-04-06 01:50:19 +0700

    First commit

---
+++ src/entrance/entrance.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    var borderline = document.getElementById('borderline');
+    borderline.classList.add('animate-borderline');
+  }, 3000);
+
+setTimeout(function() {
+    console.log('Pop!');
+    // alert('Pop!')
+  }, 3000);
+  
+
