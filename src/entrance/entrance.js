e2c01e1dbae0c29590fee75d8c38a2bb499b0bee
Watcharawish Onsanit <watcharawish@Watcharawishs-MacBook-Pro.local>
2024-04-06 02:14:30 +0700

    Finish Text and language animation

--- src/entrance/entrance.js
+++ src/entrance/entrance.js
@@ -4,8 +4,15 @@ setTimeout(function() {
   }, 3000);
 
 setTimeout(function() {
+    var language = document.getElementById('language');
+    language.classList.add('animate-language');
+  }, 3000);
+
+setTimeout(function() {
     console.log('Pop!');
     // alert('Pop!')
   }, 3000);
+
+
   
 
